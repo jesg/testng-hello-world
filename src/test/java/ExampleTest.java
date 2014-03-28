f9d28817ac58eb019dce322f3de720e8d673b076
jesg <savagejas@gmail.com>
2014-03-28 11:33:08 -0700

    initial commit

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class ExampleTest {
+    
+    @Test
+    public void exampleTest(){
+        assertEquals(2 + 2, 4, "2 + 2 should equal 4");
+    }
+
+}
